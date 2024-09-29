371843421b36ad9e5bc629cf3e6d8b4a8763051b
NotMugil <notmugil01@gmail.com>
2024-09-29 23:57:24 +0530

    refactor: move db.js to config folder
    - Renamed server/db.js to server/config/db.js
    - Updated imports in server/models/room.js
    - Updated imports in server/models/song.js
    - Updated imports in server/models/user.js

--- server/models/room.js
+++ server/models/room.js
@@ -1,4 +1,4 @@
-const db = require('../db');
+const db = require('../config/db');
 
 const Room = {
   async create(name, creatorId) {
--- server/models/song.js
+++ server/models/song.js
@@ -1,4 +1,4 @@
-const db = require('../db');
+const db = require('../config/db');
 
 const Song = {
   async addToQueue(roomId, userId, youtubeId, title) {
